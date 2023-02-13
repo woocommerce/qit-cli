589fb56f3f77941c26bcbb22f5be9ab18243220e
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-02-13 15:47:19 -0300

    Catch issues gracefully on bootstrap

--- src/qit-cli.php
+++ src/qit-cli.php
@@ -11,9 +11,9 @@ require_once __DIR__ . '/src/helpers.php';
 $container = new Container();
 App::setContainer( $container );
 
-$application = require_once __DIR__ . '/src/bootstrap.php';
-
 try {
+	$application = require_once __DIR__ . '/src/bootstrap.php';
+	
 	// Handle CLI request.
 	exit( $application->run() );
 } catch ( \Exception $e ) {
