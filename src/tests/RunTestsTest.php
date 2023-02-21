2e2fdb2eac914976e1e56c301855786f55bdcfbb
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-02-21 19:28:06 -0300

    Fix unit tests

--- src/tests/RunTestsTest.php
+++ src/tests/RunTestsTest.php
@@ -21,7 +21,10 @@ class RunTestsTest extends \QIT_CLI_Tests\QITTestCase {
 	}
 
 	public function test_run_with_additional_woo_plugins() {
-		App::setVar( sprintf( 'mock_%s', get_manager_url() . '/wp-json/cd/v1/enqueue-e2e' ), 'NULL_RESPONSE' );
+		App::setVar( sprintf( 'mock_%s', get_manager_url() . '/wp-json/cd/v1/enqueue-e2e' ), json_encode( [
+			'run_id'            => 123456,
+			'result_view_token' => ''
+		] ) );
 
 		$this->application_tester->run( [
 			'command'                  => 'run:e2e',
