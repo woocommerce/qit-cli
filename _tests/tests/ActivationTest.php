300976167f2b791f110ccfec38d693d7738c3024
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-03-21 15:42:28 -0300

    Setup workflows to support multiple self-tests

--- _tests/tests/ActivationTest.php
+++ _tests/tests/ActivationTest.php
@@ -7,7 +7,11 @@ use Spatie\Snapshots\MatchesSnapshots;
 class ActivationTest extends QITE2ETestCase {
 	use MatchesSnapshots;
 
-	public function test_security_tests() {
-		$this->assertMatchesSnapshot( $this->validate_and_normalize( __DIR__ . '/../activation.txt' ) );
+	public function test_main_activation() {
+		$this->assertMatchesSnapshot( $this->validate_and_normalize( __DIR__ . '/../activation-main.txt' ) );
+	}
+
+	public function test_php82_activation() {
+		$this->assertMatchesSnapshot( $this->validate_and_normalize( __DIR__ . '/../activation-php82.txt' ) );
 	}
 }
