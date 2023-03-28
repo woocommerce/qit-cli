1e1777ed57fa3f05c8babe198c9a42e1fad52f35
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-03-28 14:44:32 -0300

    Undo test failure in CI

--- _tests/security/main/woocommerce-product-feeds/woocommerce-product-feeds.php
+++ _tests/security/main/woocommerce-product-feeds/woocommerce-product-feeds.php
@@ -8,10 +8,8 @@ add_action( 'init', static function() {
 	if ( isset( $_POST['foo'] ) ) {
 		$foo = $_POST['foo']; // Detected usage of a non-sanitized input variable: $_POST['foo']
 		$bar = $_POST['bar']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-		$baz = $_POST['baz'];
 
 		echo "Unescaped output! $foo"; // All output should be run through an escaping function
 		echo "Unescaped output! $bar"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo "Unescaped output! $baz";
 	}
 } );
