6f481bf825f50666b24bc35b6ca2920d76684d66
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-03-28 10:23:38 -0300

    Add documentation and cleanup tests

--- _tests/QITSelfTests.php
+++ _tests/QITSelfTests.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * Check the README.md for documentation about this file.
+ *
+ * - Iterates over each test in each test-type
+ * - Create a zip file for the SUT
+ * - Create a PHPUnit test file for each test
+ * - Run all tests in parallel
+ * - Checks that the result matches the snapshot
+ */
 
 class Context {
 	public static $action;
@@ -116,11 +125,11 @@ function generate_test_runs( array $test_types ): array {
 
 function run_test_runs( array $test_runs ) {
 	foreach ( $test_runs as $test_type => $test_type_test_runs ) {
-		generate_test_files( $test_type, $test_type_test_runs );
+		generate_phpunit_files( $test_type, $test_type_test_runs );
 	}
 
 	foreach ( $test_runs as $test_type => $test_type_test_runs ) {
-		make_zips( $test_type_test_runs );
+		generate_zips( $test_type_test_runs );
 	}
 
 	$processes = [];
@@ -166,8 +175,16 @@ function run_test_runs( array $test_runs ) {
 	}
 
 	$process_manager = new \Jack\Symfony\ProcessManager();
-	$process_manager->runParallel( $processes, 10, 1000, function ( string $type, string $out, \Symfony\Component\Process\Process $process ) {
+	$process_manager->runParallel( $processes, 10, 1000, function ( string $type, string $out, \Symfony\Component\Process\Process $process ) use ( $test_runs ) {
+		/*
+		 * This is the callback that will be called when a process generates output.
+		 */
 		if ( ! is_null( json_decode( $out, true ) ) ) {
+			/*
+			 * Here we received a JSON output, so it must be the result.
+			 * - Save the output in a file.
+			 * - Run the PHPUnit test.
+			 */
 			$snapshot_filepath = $process->getEnv()['QIT_TEST_PATH'] . '/test-result.json';
 
 			if ( file_exists( $snapshot_filepath ) ) {
@@ -196,16 +213,21 @@ function run_test_runs( array $test_runs ) {
 			$process_id = $process->getPid();
 
 			$test_process = new Symfony\Component\Process\Process( $args );
-			$test_process->mustRun( function ( $type, $out ) use ( $process_id ) {
+			$exit_status_code = $test_process->run( function ( $type, $out ) use ( $process_id ) {
 				echo "[Process $process_id]: $out\n";
 			} );
+
+			foreach ( $test_runs as $test_type => $test_type_test_runs ) {
+				cleanup_test( $process->getEnv()['QIT_TEST_PATH'] );
+			}
 		} else {
+			// Not a JSON, so just output it.
 			echo "[Process {$process->getPid()}]: $out\n";
 		}
 	} );
 }
 
-function generate_test_files( string $test_type, array $test_runs ): void {
+function generate_phpunit_files( string $test_type, array $test_runs ): void {
 	$name     = ucfirst( $test_type ) . 'Test';
 	$filepath = __DIR__ . '/tests/' . $name . '.php';
 	$tests    = '';
@@ -246,13 +268,11 @@ PHP;
 	}
 }
 
-function make_zips( array $test_type_test_runs ) {
+function generate_zips( array $test_type_test_runs ) {
 	$processes = [];
 	foreach ( $test_type_test_runs as $t ) {
 		$path = $t['path'];
 		$slug = Context::$sut_slug;
-
-		$relative_path = str_replace( __DIR__, '', $path );
 
 		$args = [
 			"docker",
@@ -277,4 +297,18 @@ function make_zips( array $test_type_test_runs ) {
 	$process_manager->runParallel( $processes, 10, 1000, function ( string $type, string $out, \Symfony\Component\Process\Process $process ) {
 		echo $out;
 	} );
+}
+
+function cleanup_test( $test_type_path ) {
+	foreach ( [ 'test-result.json', 'sut.zip' ] as $file ) {
+		if ( file_exists( "$test_type_path/$file" ) ) {
+			if ( ! unlink( "$test_type_path/$file" ) ) {
+				throw new RuntimeException( "Failed to delete test result file: $test_type_path/$file" );
+			} else {
+				echo "[Cleanup] Deleted file $test_type_path/$file";
+			}
+		} else {
+			echo "[Cleanup] File does not exist $test_type_path/$file";
+		}
+	}
 }
--- _tests/README.md
+++ _tests/README.md
@@ -1,10 +1,22 @@
-## Quality Insights Toolkit end-to-end tests
+## Quality Insights Toolkit self-tests
 
-This directory contains end-to-end tests for the Quality Insights Toolkit.
+This directory contains self-tests for the Quality Insights Toolkit.
 
-### How to re-generate the snapshots:
+### Self-tests
+- Each directory is a test-type self-test, except for "vendor" and "tests" folders, eg: `activation`, `security`, `e2e`, etc.
+- Each test-type self-test can contain multiple tests, eg: `activation/test-1`, `activation/test-2`, etc.
+- Each test has a `env.php` file, with parameters used for the test, such as PHP, WooCommerce and WordPress versions, eg: `activation/test-1/env.php`.
+- The `QITSelfTests.php` file is the runner for the self-tests.
 
-Requirements: QIT CLI with `dev` mode and `production` environment
+### QITSelfTests.php 
 
-- `make regenerate_security_snapshot` to re-generate security test snapshots. 
-- `make regenerate_activation_snapshot` to re-generate activation test snapshots. 
+- For each test type, it will create the zip file of the plugin of each test.
+- For each test type, it will dispatch a QIT test with `--wait` and `--json` parameters, which will wait for the test to finish, and get the result of the test as a JSON.
+- All tests are dispatched in parallel, and the results are processed as they come in.
+- When a result is available, it will compare the JSON result with a previously saved snapshot.
+
+### Regenerating snapshots
+To re-generate the Snapshots, run `php QitSelfTests.php update`
+
+### Running only a single test type
+To run only a single test type, run `php QitSelfTests.php run TEST_TYPE`. For example, to run only the `activation` tests, run `php QitSelfTests.php run activation`. You can also `update` instead of `run`.
