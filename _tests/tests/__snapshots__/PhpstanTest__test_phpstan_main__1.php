d3ab119dfee6c7a577a9482adfbda2f72e1bf381
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-03-30 16:11:31 -0300

    Add PHPStan test

---
+++ _tests/tests/__snapshots__/PhpstanTest__test_phpstan_main__1.php
@@ -0,0 +1,30 @@
+<?php return '{
+    "run_id": 123456,
+    "test_type": "phpstan",
+    "wordpress_version": "6.0.0-normalized",
+    "woocommerce_version": "6.0.0-normalized",
+    "php_version": "7.4",
+    "additional_woo_plugins": [],
+    "additional_wp_plugins": [],
+    "test_log": "",
+    "test_result_json": "{\\"totals\\":{\\"errors\\":0,\\"file_errors\\":2},\\"files\\":{\\"\\/home\\/runner\\/work\\/compatibility-dashboard\\/compatibility-dashboard\\/ci\\/plugins\\/woocommerce-product-feeds\\/woocommerce-product-feeds.php\\":{\\"errors\\":2,\\"messages\\":[{\\"message\\":\\"Instantiated class Bar not found.\\",\\"line\\":21,\\"ignorable\\":true},{\\"message\\":\\"Result of function example_return_void (void) is used.\\",\\"line\\":22,\\"ignorable\\":true}]}},\\"errors\\":[]}",
+    "status": "failed",
+    "test_result_aws_url": "https:\\/\\/test-results-aws.com",
+    "test_result_aws_expiration": 1234567890,
+    "is_development": true,
+    "woo_extension": {
+        "id": 18619,
+        "host": "wccom",
+        "name": "Google Product Feed"
+    },
+    "client": "qit_cli",
+    "event": "cli_development_extension_test",
+    "optional_features": {
+        "hpos": false,
+        "cc_blocks": false
+    },
+    "test_results_manager_url": "https:\\/\\/test-results-manager.com",
+    "test_results_manager_expiration": 1234567890,
+    "test_summary": "Errors: 0, File Errors: 2",
+    "version": "Zip"
+}';
