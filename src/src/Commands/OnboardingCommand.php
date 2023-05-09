2a8e0e9a55d66a74420c362c251adb5bca56ae1c
Lucas Bustamante <lucasfbustamante@gmail.com>
2023-05-09 12:43:13 -0300

    Phan

--- src/src/Commands/OnboardingCommand.php
+++ src/src/Commands/OnboardingCommand.php
@@ -10,7 +10,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Terminal;
