0f1d61c5de05ac9e9309a41e9eb2835831863d94
jamelreid <jnoelreid@gmail.com>
2023-05-24 08:55:45 -0500

    Removed unnecessary var

--- _tests/api/valid_features/woocommerce-product-feeds/woocommerce-product-feeds.php
+++ _tests/api/valid_features/woocommerce-product-feeds/woocommerce-product-feeds.php
@@ -5,7 +5,6 @@
  */
 
  add_action( 'woocommerce_admin_get_feature_config', static function( $features ) {
-	$features_controller = wc_get_container()->get( 'Automattic\WooCommerce\Internal\Features\FeaturesController' );
 	if ( 
 		array_key_exists( 'new-product-management-experience', $features ) &&
 		$features['new-product-management-experience'] === true
