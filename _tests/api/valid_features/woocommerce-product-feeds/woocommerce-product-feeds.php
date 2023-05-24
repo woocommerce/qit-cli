3b069094291448b53f5a12c663d050ef438c5c05
jamelreid <jnoelreid@gmail.com>
2023-05-24 09:30:27 -0500

    Used add_filter

--- _tests/api/valid_features/woocommerce-product-feeds/woocommerce-product-feeds.php
+++ _tests/api/valid_features/woocommerce-product-feeds/woocommerce-product-feeds.php
@@ -4,7 +4,7 @@
  * Plugin name: API - Log that the  New Product Editor is enabled.
  */
 
- add_action( 'woocommerce_admin_get_feature_config', static function( $features ) {
+ add_filter( 'woocommerce_admin_get_feature_config', static function( $features ) {
 	if ( 
 		array_key_exists( 'new-product-management-experience', $features ) &&
 		$features['new-product-management-experience'] === true
