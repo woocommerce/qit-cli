5b0a2b06c350599b09d87ac8bc6625eee99e43fd
Greg <zhongruige@gmail.com>
2023-08-03 09:58:24 -0600

    Update self tests snapshots

--- _tests/tests/__snapshots__/E2eTest__test_e2e_no_op_php82__1.php
+++ _tests/tests/__snapshots__/E2eTest__test_e2e_no_op_php82__1.php
@@ -9,7 +9,7 @@
             "additional_woo_plugins": [],
             "additional_wp_plugins": [],
             "test_log": "",
-            "status": "success",
+            "status": "failed",
             "test_result_aws_url": "https:\\/\\/test-results-aws.com",
             "test_result_aws_expiration": 1234567890,
             "is_development": true,
@@ -27,7 +27,7 @@
             },
             "test_results_manager_url": "https:\\/\\/test-results-manager.com",
             "test_results_manager_expiration": 1234567890,
-            "test_summary": "Test Suites: 0 skipped, 0 failed, 49 passed, 49 total | Tests: 17 skipped, 0 failed, 186 passed, 203 total.",
+            "test_summary": "Test Suites: 0 skipped, 1 failed, 48 passed, 49 total | Tests: 17 skipped, 1 failed, 185 passed, 203 total.",
             "version": "Undefined",
             "update_complete": true,
             "ai_suggestion_status": "none",
@@ -35,12 +35,12 @@
         },
         {
             "test_result_json": {
-                "numFailedTestSuites": 0,
-                "numPassedTestSuites": 49,
+                "numFailedTestSuites": 1,
+                "numPassedTestSuites": 48,
                 "numPendingTestSuites": 0,
                 "numTotalTestSuites": 49,
-                "numFailedTests": 0,
-                "numPassedTests": 186,
+                "numFailedTests": 1,
+                "numPassedTests": 185,
                 "numPendingTests": 17,
                 "numTotalTests": 203,
                 "testResults": [
@@ -1123,7 +1123,7 @@
                     },
                     {
                         "file": "shopper\\/checkout.spec.js",
-                        "status": "passed",
+                        "status": "failed",
                         "has_pending": false,
                         "tests": {
                             "Checkout page": [
@@ -1145,7 +1145,7 @@
                                 },
                                 {
                                     "title": "allows guest customer to place an order",
-                                    "status": "passed"
+                                    "status": "failed"
                                 },
                                 {
                                     "title": "allows existing customer to place order",
@@ -1327,7 +1327,7 @@
                         }
                     }
                 ],
-                "summary": "Test Suites: 0 skipped, 0 failed, 49 passed, 49 total | Tests: 17 skipped, 0 failed, 186 passed, 203 total."
+                "summary": "Test Suites: 0 skipped, 1 failed, 48 passed, 49 total | Tests: 17 skipped, 1 failed, 185 passed, 203 total."
             }
         }
     ]
