98efbf9ee19985a5a097085979bc6abc54048d9b
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-02-09 15:29:53 -0300

    E2E snapshots

---
+++ _tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_no_op_php82_woostable_php82_wprc_960168535790f3a7eff463fb11517d63__1.php
@@ -0,0 +1,65 @@
+<?php return '[
+    [
+        {
+            "test_run_id": 123456,
+            "run_id": 123456,
+            "test_type": "woo-e2e",
+            "wordpress_version": "6.0.0-normalized",
+            "woocommerce_version": "6.0.0-normalized",
+            "php_version": "8.2",
+            "max_php_version": "",
+            "min_php_version": "",
+            "additional_woo_plugins": [],
+            "additional_wp_plugins": [],
+            "test_log": "",
+            "status": "success",
+            "test_result_aws_url": "https:\\/\\/test-results-aws.com",
+            "test_result_aws_expiration": 1234567890,
+            "is_development": true,
+            "send_notifications": false,
+            "woo_extension": {
+                "id": 18619,
+                "host": "wccom",
+                "name": "Google Product Feed",
+                "type": "plugin"
+            },
+            "client": "qit_cli",
+            "event": "cli_development_extension_test",
+            "optional_features": {
+                "hpos": false,
+                "new_product_editor": false
+            },
+            "test_results_manager_url": "https:\\/\\/test-results-manager.com",
+            "test_results_manager_expiration": 1234567890,
+            "test_summary": "Test failed before it was executed.",
+            "version": "Undefined",
+            "update_complete": true,
+            "ai_suggestion_status": "none",
+            "malware_whitelist_paths": [],
+            "test_result_json_extracted": "{EXTRACTED}",
+            "debug_log_extracted": "{EXTRACTED}"
+        },
+        {
+            "test_result_json": {
+                "numFailedTestSuites": 0,
+                "numPassedTestSuites": 0,
+                "numPendingTestSuites": 0,
+                "numTotalTestSuites": 0,
+                "numFailedTests": 0,
+                "numPassedTests": 0,
+                "numPendingTests": 0,
+                "numTotalTests": 0,
+                "testResults": [],
+                "summary": "Test failed before it was executed."
+            }
+        },
+        {
+            "debug_log": [
+                {
+                    "count": "1",
+                    "message": "PHP Deprecated: strlen(): Passing null to parameter #1 ($string) of type string is deprecated in \\/var\\/www\\/html\\/wp-includes\\/formatting.php on line 3771"
+                }
+            ]
+        }
+    ]
+]';
