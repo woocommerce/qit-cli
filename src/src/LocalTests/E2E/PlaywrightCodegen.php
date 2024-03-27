c60223841e8607228fdf760b9c23890482ee2d7c
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-03-27 13:29:35 -0300

    Handle gracefully if npm install playwright fails for codegen

--- src/src/LocalTests/E2E/PlaywrightCodegen.php
+++ src/src/LocalTests/E2E/PlaywrightCodegen.php
@@ -2,11 +2,23 @@
 
 namespace QIT_CLI\LocalTests\E2E;
 
+use QIT_CLI\App;
 use QIT_CLI\Config;
 use QIT_CLI\Environment\Environments\E2E\E2EEnvInfo;
+use QIT_CLI\IO\Input;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class PlaywrightCodegen {
+	/** @var OutputInterface $output */
+	protected $output;
+
+	public function __construct( OutputInterface $output ) {
+		$this->output = $output;
+	}
+
 	public function open_codegen( E2EEnvInfo $env_info ): void {
 		if ( ! file_exists( Config::get_qit_dir() . 'playwright-codegen' ) ) {
 			if ( ! mkdir( Config::get_qit_dir() . 'playwright-codegen', 0755, true ) ) {
@@ -28,15 +40,46 @@ class PlaywrightCodegen {
 		}
 
 		// Install Playwright in the config directory.
-		$process = new Process( [ 'npm', 'install', 'playwright' ], Config::get_qit_dir() . 'playwright-codegen' );
-		$process->setTimeout( 600 );
-		$process->setIdleTimeout( 600 );
-		$process->run();
+		$process = new Process( [ 'npm', 'install', 'playwright', '--verbose' ], Config::get_qit_dir() . 'playwright-codegen' );
+		$process->setTimeout( 60 );
+		$process->setIdleTimeout( 60 );
+		$process->run( function ( $type, $buffer ) {
+			if ( $this->output->isVerbose() ) {
+				$this->output->write( $buffer );
+			}
+		} );
+
+		if ( ! $process->isSuccessful() ) {
+			/**
+			 * Sometimes, "npm install" might fail due to network issues that are completely out of our control, such as:
+			 *
+			 * - NPM Registry using IPV6 and the host being unable to handle it.
+			 * - The host having incorrect clock time that might cause SSL handshake to fail.
+			 * - Network issues.
+			 * - Proxy.
+			 * - VPN.
+			 * - Being blocked by the NPM registry.
+			 *
+			 * So we give them an alternative in this scenario.
+			 *
+			 * @link https://stackoverflow.com/questions/16873973/npm-install-hangs
+			 * @link https://github.com/npm/cli/issues/3257
+			 * @link https://stackoverflow.com/questions/70678583/stop-npm-install-at-idealtreeregal-sill-idealtree-bui
+			 */
+			$this->output->writeln( '<comment>Playwright installation failed (timeout after 60 seconds). Run Playwright Codegen manually and connect to the environment above. You can also manually install Playwright in the expected directory by running `npm install playwright` in ' . Config::get_qit_dir() . 'playwright-codegen. For detailed output, reattempt with "--verbose". This issue may be network-related (e.g., proxy, VPN).</comment>' );
+
+			// Hold the environment up until user interacts.
+			( new QuestionHelper() )->ask( App::make( Input::class ), $this->output, new ConfirmationQuestion( 'Press Enter to continue...', false ) );
+
+			return;
+		}
 
 		// Open Codegen.
 		$process = new Process( [ 'npx', 'playwright', 'codegen', $env_info->site_url ], Config::get_qit_dir() . 'playwright-codegen' );
 		$process->setTimeout( null );
 		$process->setIdleTimeout( null );
-		$process->run();
+		$process->run( function ( $type, $buffer ) {
+			$this->output->write( $buffer );
+		} );
 	}
 }
