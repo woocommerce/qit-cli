2f84781ab2198f3d3ed206fcf3af37e125d12f36
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-05-02 18:12:49 -0300

    Debug CI

--- _tests/custom_tests/tests/Traits/SnapshotHelpers.php
+++ _tests/custom_tests/tests/Traits/SnapshotHelpers.php
@@ -9,6 +9,15 @@ trait SnapshotHelpers {
 
 	public function assertMatchesNormalizedSnapshot( $actual, ?\Spatie\Snapshots\Driver $driver = null ): void {
 		$actual = str_replace( sys_get_temp_dir(), '/tmp-normalized', $actual );
+
+		/*
+		 * "paratest" sets the "TEST_TOKEN" env var.
+		 * If this is not set, it means we are running in a normal PHPUnit environment.
+		 */
+		if ( empty( getenv( 'TEST_TOKEN' ) ) ) {
+			$actual = preg_replace( '/First-time setup is pulling Docker images and caching downloads. Subsequent runs will be faster.\n/', '', $actual );
+		}
+		
 		$this->assertMatchesSnapshot( $actual, $driver );
 	}
 }
