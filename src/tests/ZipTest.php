aafd7c269e92faf71a7ddb37b6bacfeee80093a1
Rommel Castro <rommelxcastro@gmail.com>
2024-05-03 03:30:10 -0600

    test `\QIT_CLI\Zip`

--- src/tests/ZipTest.php
+++ src/tests/ZipTest.php
@@ -16,6 +16,9 @@ class ZipTest extends QITTestCase {
 		file_put_contents( __DIR__ . '/plugin-entrypoint.php', '<?php Plugin Name: Foo' );
 		$this->to_delete[] = __DIR__ . '/plugin-entrypoint.php';
 
+		file_put_contents( __DIR__ . '/Thumbs.db', '' );
+		$this->to_delete[] = __DIR__ . '/Thumbs.db';
+
 		parent::setUp();
 	}
 
@@ -44,6 +47,63 @@ class ZipTest extends QITTestCase {
 			$zip->validate_zip( $zip_file );
 		} catch ( \Exception $e ) {
 			$this->fail( 'The zip file should be valid.' );
+		}
+
+		$this->assertTrue( true );
+	}
+
+	public function test_zip_with_invalid_file() {
+		$this->inject_request_builder_mock();
+
+		$file_name = 'zip-with-invalid-file.zip';
+		$slug      = 'zip-with-invalid-file';
+
+		$zip      = App::make( Zip::class );
+		$zip_file = ( new ZipBuilder( $file_name ) )
+			->with_file( __DIR__ . '/plugin-entrypoint.php', $slug . '/plugin-entrypoint.php' )
+			->with_file( __DIR__ . '/Thumbs.db', $slug . '/Thumbs.db' )
+			->build();
+
+		$this->expectException( \RuntimeException::class );
+		$this->expectExceptionMessage( 'Invalid (zip-with-invalid-file/Thumbs.db) file/folder inside zip file' );
+
+		$zip->validate_zip( $zip_file );
+	}
+
+	function test_zip_with_invalid_file_inside_folder() {
+		$this->inject_request_builder_mock();
+
+		$file_name = 'zip-with-invalid-file-inside-folder.zip';
+		$slug      = 'zip-with-invalid-file-inside-folder';
+
+		$zip      = App::make( Zip::class );
+		$zip_file = ( new ZipBuilder( $file_name ) )
+			->with_file( __DIR__ . '/plugin-entrypoint.php', $slug . '/plugin-entrypoint.php' )
+			->with_file( __DIR__ . '/Thumbs.db', $slug . '/foo/Thumbs.db' )
+			->build();
+
+		$this->expectException( \RuntimeException::class );
+		$this->expectExceptionMessage( 'Invalid (zip-with-invalid-file-inside-folder/foo/Thumbs.db) file/folder inside zip file' );
+
+		$zip->validate_zip( $zip_file );
+	}
+
+	function test_zip_with_invalid_file_inside_vendor_folder() {
+		$this->inject_request_builder_mock();
+
+		$file_name = 'zip-with-invalid-file-inside-vendor-folder.zip';
+		$slug      = 'zip-with-invalid-file-inside-vendor-folder';
+
+		$zip      = App::make( Zip::class );
+		$zip_file = ( new ZipBuilder( $file_name ) )
+			->with_file( __DIR__ . '/plugin-entrypoint.php', $slug . '/plugin-entrypoint.php' )
+			->with_file( __DIR__ . '/Thumbs.db', $slug . '/vendor/Thumbs.db' )
+			->build();
+
+		try{
+			$zip->validate_zip( $zip_file );
+		} catch (\Exception $e) {
+			$this->fail('The zip file should be valid.');
 		}
 
 		$this->assertTrue( true );
