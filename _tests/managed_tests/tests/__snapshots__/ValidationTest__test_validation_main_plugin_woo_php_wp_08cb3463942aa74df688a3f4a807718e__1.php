1d8a0cbf517204e24e12156079aa1f6449a3df8e
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-08-28 09:45:09 -0300

    Add snapshots

---
+++ _tests/managed_tests/tests/__snapshots__/ValidationTest__test_validation_main_plugin_woo_php_wp_08cb3463942aa74df688a3f4a807718e__1.php
@@ -0,0 +1,53 @@
+<?php return '[
+    [
+        {
+            "test_run_id": 123456,
+            "run_id": 123456,
+            "test_type": "validation",
+            "test_type_display": "Validation",
+            "wordpress_version": "6.0.0-normalized",
+            "woocommerce_version": "6.0.0-normalized",
+            "php_version": "7.4",
+            "max_php_version": "",
+            "min_php_version": "",
+            "additional_woo_plugins": [],
+            "additional_wp_plugins": [],
+            "test_log": "",
+            "status": "success",
+            "test_result_aws_url": "https:\\/\\/test-results-aws.com",
+            "test_result_aws_expiration": 1234567890,
+            "is_development": true,
+            "send_notifications": false,
+            "woo_extension": {
+                "id": 18619,
+                "host": "wccom",
+                "name": "Google Product Feed",
+                "type": "plugin"
+            },
+            "client": "qit_cli",
+            "event": "cli_development_extension_test",
+            "optional_features": {
+                "hpos": false,
+                "new_product_editor": false
+            },
+            "test_results_manager_url": "https:\\/\\/test-results-manager.com",
+            "test_results_manager_expiration": 1234567890,
+            "test_summary": "Errors: 0 Warnings: 0",
+            "debug_log": "",
+            "version": "Undefined",
+            "update_complete": true,
+            "ai_suggestion_status": "none",
+            "malware_whitelist_paths": [],
+            "workflow_id": "1234567890",
+            "runner": "normalized",
+            "test_result_json_extracted": "{EXTRACTED}"
+        },
+        {
+            "test_result_json": {
+                "headers": {
+                    "Requires PHP": "7.4"
+                }
+            }
+        }
+    ]
+]';
