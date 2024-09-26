d0737a3de59ea98835ed594460c1ae39603e1c4b
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-09-26 18:10:25 -0300

    Scaffold with the new orchestration structure

--- src/src/Commands/CustomTests/ScaffoldE2ECommand.php
+++ src/src/Commands/CustomTests/ScaffoldE2ECommand.php
@@ -5,6 +5,7 @@ namespace QIT_CLI\Commands\CustomTests;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use function QIT_CLI\normalize_path;
@@ -15,6 +16,7 @@ class ScaffoldE2ECommand extends Command {
 	protected function configure() {
 		$this
 			->addArgument( 'path', InputArgument::REQUIRED, 'The path to scaffold an example E2E test.' )
+			->addOption( 'advanced', 'f', InputOption::VALUE_NONE, 'Include advanced scaffolding.' )
 			->setDescription( 'Scaffold an example E2E test.' );
 	}
 
@@ -41,39 +43,48 @@ class ScaffoldE2ECommand extends Command {
 			return Command::FAILURE;
 		}
 
-		// Create basic bootstrap.
-		if ( ! mkdir( $path_to_generate . '/bootstrap', 0755, true ) ) {
-			$output->writeln( '<error>Could not create directory: ' . $path_to_generate . '/bootstrap</error>' );
+		// Create basic 'qit' directory.
+		if ( ! mkdir( $path_to_generate . '/qit', 0755, true ) ) {
+			$output->writeln( '<error>Could not create directory: ' . $path_to_generate . '/qit</error>' );
 
 			return Command::FAILURE;
 		}
 
-		// bootstrap.sh.
-		if ( ! file_put_contents( $path_to_generate . '/bootstrap/bootstrap.sh', $this->generate_bootstrap_shell_example() ) ) {
-			$output->writeln( '<error>Could not create file: ' . $path_to_generate . '/bootstrap/bootstrap.sh</error>' );
+		// We bootstrap with these files by default.
+		$files = [
+			'sharedSetup-sh.txt'     => '/qit/sharedSetup.sh',
+			'sharedSetup-js.txt'     => '/qit/sharedSetup.js',
 
-			return Command::FAILURE;
+			'isolatedSetup-sh.txt'   => '/qit/isolatedSetup.sh',
+			'isolatedSetup-js.txt'   => '/qit/isolatedSetup.js',
+
+			'mu-plugin-php.txt'      => '/qit/mu-plugin.php',
+			'dependencies-json.txt'  => '/qit/dependencies.json',
+			'example-spec-js.txt'    => '/example.spec.js',
+		];
+
+		// If the user requests the advanced bootstrapping, we include every possibility.
+		if ( $input->getOption( 'advanced' ) ) {
+			$files = array_merge( $files, [
+				'sharedSetup-php.txt' => '/qit/sharedSetup.php',
+
+				'isolatedSetup-php.txt' => '/qit/isolatedSetup.php',
+
+				'sharedTeardown-sh.txt'  => '/qit/sharedTeardown.sh',
+				'sharedTeardown-php.txt' => '/qit/sharedTeardown.php',
+				'sharedTeardown-js.txt'  => '/qit/sharedTeardown.js',
+
+				'isolatedTeardown-sh.txt'  => '/qit/isolatedTeardown.sh',
+				'isolatedTeardown-php.txt' => '/qit/isolatedTeardown.php',
+				'isolatedTeardown-js.txt'  => '/qit/isolatedTeardown.js',
+			] );
 		}
 
-		// bootstrap.php (read from text file to avoid our prefixer).
-		if ( ! file_put_contents( $path_to_generate . '/bootstrap/bootstrap.php', file_get_contents( __DIR__ . '/scaffolding/bootstrap-php.txt' ) ) ) {
-			$output->writeln( '<error>Could not create file: ' . $path_to_generate . '/bootstrap/bootstrap.php</error>' );
-
-			return Command::FAILURE;
-		}
-
-		// mu-plugin.php (read from text file to avoid our prefixer).
-		if ( ! file_put_contents( $path_to_generate . '/bootstrap/mu-plugin.php', file_get_contents( __DIR__ . '/scaffolding/mu-plugin.txt' ) ) ) {
-			$output->writeln( '<error>Could not create file: ' . $path_to_generate . '/bootstrap/mu-plugin.php</error>' );
-
-			return Command::FAILURE;
-		}
-
-		// example.spec.js.
-		if ( ! file_put_contents( $path_to_generate . '/example.spec.js', $this->example_spec_js() ) ) {
-			$output->writeln( '<error>Could not create file: ' . $path_to_generate . '/example.spec.js</error>' );
-
-			return Command::FAILURE;
+		foreach ( $files as $example => $destination ) {
+			$result = $this->create_file_from_template( $output, $path_to_generate, $example, $destination );
+			if ( $result === Command::FAILURE ) {
+				return $result;
+			}
 		}
 
 		$output->writeln( '<info>Example E2E test generated in: ' . $path_to_generate . '</info>' );
@@ -85,47 +96,13 @@ class ScaffoldE2ECommand extends Command {
 		return Command::SUCCESS;
 	}
 
-	protected function generate_bootstrap_shell_example(): string {
-		return <<<'SHELL'
-#!/bin/bash
+	function create_file_from_template( OutputInterface $output, string $path_to_generate, string $source, string $destination ): int {
+		if ( ! file_put_contents( "$path_to_generate/$destination", file_get_contents( __DIR__ . "/scaffolding/$source" ) ) ) {
+			$output->writeln( '<error>Could not create file: ' . basename( $destination ) . '</error>' );
 
-# Bootstrap Shell Script (Optional)
+			return Command::FAILURE;
+		}
 
-# Purpose: This script is executed before test runs to set up the testing environment.
-#
-# Usage:
-# - Use WP CLI to configure prerequisites for your tests. 
-# - Example: To install a specific theme required for tests:
-#   wp theme install twentytwentynine
-#   (You can then activate this theme during your tests)
-#
-# Note: Delete this file if it's not required for your setup.
-#
-# Documentation: Detailed instructions available at https://qit.woo.com/docs/custom-tests/generating-tests
-SHELL;
-	}
-
-	protected function example_spec_js(): string {
-		return <<<'JS'
-/*
- * This is an example E2E test. You can write your own tests, or generate them with Codegen.
- * 
- * Read more about it on our documentation: https://qit.woo.com/docs/custom-tests/generating-tests
- */
-import { test, expect } from '@playwright/test';
-import qit from '/qitHelpers';
-
-test('I can see my plugin menu', async ({ page }) => {
-    // Log-in as admin.
-    await qit.loginAsAdmin(page);
-    // View WordPress Core "Dashboard" heading
-    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
-    // Click on my menu on the sidebar.
-    // await page.getByRole('link', { name: 'My Plugin Menu', exact: true }).click();
-    // await page.waitForLoadState('networkidle');
-    // Assert I see my welcome message when I click it.
-    // await expect(page.locator('h3')).toContainText('Welcome to My Plugin!');
-});
-JS;
+		return Command::SUCCESS;
 	}
 }
