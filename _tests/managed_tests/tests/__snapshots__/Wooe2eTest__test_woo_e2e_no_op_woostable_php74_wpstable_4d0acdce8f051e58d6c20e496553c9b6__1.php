77854cd178d75682d287911f63b57d15af9d742a
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-10-04 08:58:16 -0300

    Update snapshot

--- _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_no_op_woostable_php74_wpstable_4d0acdce8f051e58d6c20e496553c9b6__1.php
+++ _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_no_op_woostable_php74_wpstable_4d0acdce8f051e58d6c20e496553c9b6__1.php
@@ -33,7 +33,6 @@
             "test_results_manager_url": "https:\\/\\/test-results-manager.com",
             "test_results_manager_expiration": 1234567890,
             "test_summary": "442 total, 399 passed, 0 failed, 43 skipped.",
-            "debug_log": "",
             "version": "Undefined",
             "update_complete": true,
             "ai_suggestion_status": "none",
@@ -41,7 +40,8 @@
             "workflow_id": "1234567890",
             "runner": "normalized",
             "test_media": [],
-            "test_result_json_extracted": "{EXTRACTED}"
+            "test_result_json_extracted": "{EXTRACTED}",
+            "debug_log_extracted": "{EXTRACTED}"\n
         },
         {
             "test_result_json": {
@@ -2910,6 +2910,11 @@
                     }
                 ],
                 "summary": "442 total, 399 passed, 0 failed, 43 skipped."
+            }
+        },
+        {
+            "debug_log": {
+                "generic": []
             }
         }
     ]
