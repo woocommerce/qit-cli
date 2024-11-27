4e753d00a2ce96a97cbceb9f83ae0c754801d888
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-11-27 19:54:46 -0300

    Increase tolerance for very high number of debug logs

--- _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_no_op_woorc_php74_wpstable_8ea880f7c0e5f4aeb8851ca23268fcd8__1.php
+++ _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_no_op_woorc_php74_wpstable_8ea880f7c0e5f4aeb8851ca23268fcd8__1.php
@@ -2844,7 +2844,7 @@
             "debug_log": {
                 "generic": [
                     {
-                        "count": "13000",
+                        "count": "10000",
                         "message": "PHP Notice: Function _load_textdomain_just_in_time was called incorrectly. Translation loading for the wp-mail-logging domain was triggered too early. This is usually an indicator for some code in the plugin or theme running too early. Translations should be loaded at the init action or later. Please see Debugging in WordPress for more information. (This message was added in version 6.7.0.) in \\/var\\/www\\/html\\/wp-includes\\/functions.php on line 6114"
                     }
                 ]
