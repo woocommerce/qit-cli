c8b5508bf40daf4f195251e07676c71c71d5ab2b
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-12-19 00:53:06 -0300

    Update snapshots

--- _tests/managed_tests/tests/__snapshots__/WooapiTest__test_woo_api_order_cache_bug_woorc_php74_wprc_ffc30a443f4fcc32b0abe133375adbd8__1.php
+++ _tests/managed_tests/tests/__snapshots__/WooapiTest__test_woo_api_order_cache_bug_woorc_php74_wprc_ffc30a443f4fcc32b0abe133375adbd8__1.php
@@ -62,7 +62,7 @@
                             "Coupons API tests": [
                                 {
                                     "title": "can create a coupon",
-                                    "status": "failed"
+                                    "status": "passed"
                                 },
                                 {
                                     "title": "can retrieve a coupon",
