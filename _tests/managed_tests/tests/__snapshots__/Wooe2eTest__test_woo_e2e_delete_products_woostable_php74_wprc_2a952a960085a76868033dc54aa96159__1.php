fbf32fb6846f0705322743a74be3b2658fb420d5
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-01-08 16:16:41 -0300

    Snapshot updates

--- _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_delete_products_woostable_php74_wprc_2a952a960085a76868033dc54aa96159__1.php
+++ _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_delete_products_woostable_php74_wprc_2a952a960085a76868033dc54aa96159__1.php
@@ -33,6 +33,7 @@
             "test_results_manager_url": "https:\\/\\/test-results-manager.com",
             "test_results_manager_expiration": 1234567890,
             "test_summary": "Delete_Products Normalized Summary",
+            "debug_log": "",
             "version": "Undefined",
             "update_complete": true,
             "ai_suggestion_status": "none",
@@ -40,19 +41,10 @@
             "workflow_id": "1234567890",
             "runner": "normalized",
             "test_media": [],
-            "test_result_json_extracted": "{EXTRACTED}",
-            "debug_log_extracted": "{EXTRACTED}"
+            "test_result_json_extracted": "{EXTRACTED}"
         },
         {
             "test_result_json": []
-        },
-        {
-            "debug_log": [
-                {
-                    "count": "0",
-                    "message": "Debug log is ignored for woo-e2e\\/delete_products tests."
-                }
-            ]
         }
     ]
 ]';
