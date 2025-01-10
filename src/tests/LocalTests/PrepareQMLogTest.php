ef0027cb9acff4c2cd32c262a5c5c2fd2eaf80d1
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-01-10 15:19:30 -0300

    Update snapshot

--- src/tests/LocalTests/PrepareQMLogTest.php
+++ src/tests/LocalTests/PrepareQMLogTest.php
@@ -12,9 +12,9 @@ class PrepareQMLogTest extends QITTestCase {
 
 	public function test_parses_fatal_and_parse_errors() {
 		$debug_log = <<<'TXT'
-[10-Jan-2025 17:12:43 UTC] PHP Parse error:  syntax error, unexpected '|', expecting variable (T_VARIABLE) in /var/www/html/wp-content/plugins/kymvex-inv-alert/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php on line 83
+[10-Jan-2025 17:12:43 UTC] PHP Parse error:  syntax error, unexpected '|', expecting variable (T_VARIABLE) in /var/www/html/wp-content/plugins/foo-slug/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php on line 83
 [10-Jan-2025 17:12:43 UTC] PHP Fatal error:  Exception thrown without a stack frame in Unknown on line 0
-[10-Jan-2025 17:52:22 UTC] PHP Fatal error:  Uncaught Exception: Foo in /var/www/html/wp-content/plugins/kymvex-inv-alert/kymvex-inv-alert.php:29
+[10-Jan-2025 17:52:22 UTC] PHP Fatal error:  Uncaught Exception: Foo in /var/www/html/wp-content/plugins/foo-slug/foo-slug.php:29
 TXT;
 		$sut = App::make( PrepareQMLog::class );
 
