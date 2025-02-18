ba93be38cbb0b50ea87c1d79494db51ddd7ac6f3
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-02-18 11:45:46 -0300

    Woo E2E snapshots

--- _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_delete_products_woostable_php74_wprc_2a952a960085a76868033dc54aa96159__1.php
+++ _tests/managed_tests/tests/__snapshots__/Wooe2eTest__test_woo_e2e_delete_products_woostable_php74_wprc_2a952a960085a76868033dc54aa96159__1.php
@@ -47,6 +47,7 @@
             "runner": "normalized",
             "test_media": [],
             "extension_set": "",
+            "phpstan_level": null,
             "test_result_json_extracted": "{EXTRACTED}",
             "ctrf_json_extracted": "{EXTRACTED}"
         },
