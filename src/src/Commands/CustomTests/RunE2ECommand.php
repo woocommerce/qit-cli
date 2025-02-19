9bbaeaff1eb6e0dd456854cc1510f784810263ad
MrJnrman <jnoelreid@gmai.com>
2025-02-19 14:02:11 -0500

    Updated tag name

--- src/src/Commands/CustomTests/RunE2ECommand.php
+++ src/src/Commands/CustomTests/RunE2ECommand.php
@@ -114,7 +114,7 @@ class RunE2ECommand extends DynamicCommand {
 			->addArgument( 'test', InputArgument::OPTIONAL, '(Optional) The tests for the main extension under test. Accepts test tags, or a test directory. If not set, will use the "default" test tag of this extension.' )
 			->addOption( 'source', null, InputOption::VALUE_OPTIONAL, 'The source of the main extension under test. Accepts a slug, a file, a URL. If not provided, the source will be the slug.' )
 			->addOption( 'sut_action', null, InputOption::VALUE_OPTIONAL, 'What action to take on the SUT. Possible values: ' . implode( ', ', Extension::ACTIONS ), Extension::ACTIONS['test'] )
-			->addOption( 'pw_test_tag', null, InputOption::VALUE_OPTIONAL, 'The Playwright test tag to run.' )
+			->addOption( 'pw_test_tag', null, InputOption::VALUE_OPTIONAL, 'The Playwright test tag to run.', 'full' )
 			->reuseOption( UpEnvironmentCommand::getDefaultName(), 'wp' )
 			->reuseOption( UpEnvironmentCommand::getDefaultName(), 'woo' )
 			->reuseOption( UpEnvironmentCommand::getDefaultName(), 'plugin' )
--- src/src/LocalTests/E2E/Runner/PlaywrightRunner.php
+++ src/src/LocalTests/E2E/Runner/PlaywrightRunner.php
@@ -208,7 +208,7 @@ class PlaywrightRunner extends E2ERunner {
 
 		$options = App::getVar( 'pw_options', '' );
 
-		$options .= ' --grep "@' . $env_info->pw_test_tag . '"';
+		$options .= ' --grep @' . $env_info->pw_test_tag;
 
 		if ( $test_mode === 'headed' ) {
 			$options .= ' --headed --ui-port=8086 --ui-host=0.0.0.0';
@@ -235,6 +235,8 @@ class PlaywrightRunner extends E2ERunner {
 			"cd /qit/tests/e2e $dependencies_command" .
 			"npx playwright test $options --config /qit/tests/e2e/qit-playwright.config.js $shard 2>&1",
 		] );
+
+		$this->output->writeln( "npx playwright test $options --config /qit/tests/e2e/qit-playwright.config.js $shard 2>&1" );
 
 		// Make sure the Playwright image is up-to-date.
 		App::make( Docker::class )->maybe_pull_image( "automattic/qit-runner-playwright:$playwright_version_to_use" );
