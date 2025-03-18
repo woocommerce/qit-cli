80989401ebf24ccce91c37a1f5e044597f42161f
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-03-18 19:18:33 -0300

    Graceful handling of missing Docker scenario

--- src/src/Commands/Environment/UpEnvironmentCommand.php
+++ src/src/Commands/Environment/UpEnvironmentCommand.php
@@ -5,6 +5,7 @@ namespace QIT_CLI\Commands\Environment;
 use Dotenv\Dotenv;
 use QIT_CLI\App;
 use QIT_CLI\Cache;
+use QIT_CLI\Environment\Docker;
 use QIT_CLI\ExtensionSetResolver;
 use QIT_CLI\Commands\DynamicCommand;
 use QIT_CLI\Commands\DynamicCommandCreator;
@@ -196,6 +197,14 @@ HELP
 	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		if ( is_windows() ) {
 			$output->writeln( '<comment>To use QIT Environments on Window, please use WSL. Check our guide here: https://qit.woo.com/docs/environment/getting-started#getting-started---windows</comment>' );
+
+			return Command::FAILURE;
+		}
+
+		try {
+			App::make( Docker::class )->find_docker();
+		} catch ( \Exception $e ) {
+			$output->writeln( '<error>Please install Docker to use this command.</error>' );
 
 			return Command::FAILURE;
 		}
