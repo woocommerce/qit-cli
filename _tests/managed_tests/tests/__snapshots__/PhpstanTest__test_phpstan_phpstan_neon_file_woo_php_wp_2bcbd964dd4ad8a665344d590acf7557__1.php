61167ab810203d209d1534995dc13cfb1828a06f
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-03-19 15:25:50 -0300

    Self-tests around PHPStan neon files

---
+++ _tests/managed_tests/tests/__snapshots__/PhpstanTest__test_phpstan_phpstan_neon_file_woo_php_wp_2bcbd964dd4ad8a665344d590acf7557__1.php
@@ -0,0 +1,81 @@
+<?php return '[
+    [
+        {
+            "test_run_id": 123456,
+            "run_id": 123456,
+            "test_type": "phpstan",
+            "test_type_display": "PHPStan",
+            "wordpress_version": "6.0.0-normalized",
+            "woocommerce_version": "6.0.0-normalized",
+            "php_version": "7.4",
+            "max_php_version": "",
+            "min_php_version": "",
+            "additional_woo_plugins": [],
+            "additional_wp_plugins": [],
+            "test_log": "",
+            "ctrf_json": "",
+            "status": "failed",
+            "test_result_aws_url": "https:\\/\\/test-results-aws.com",
+            "test_result_aws_expiration": 1234567890,
+            "is_development": true,
+            "send_notifications": false,
+            "woo_extension": {
+                "id": 18619,
+                "host": "wccom",
+                "name": "Google Product Feed",
+                "type": "plugin"
+            },
+            "client": "qit_cli",
+            "event": "cli_development_extension_test",
+            "optional_features": {
+                "hpos": false,
+                "new_product_editor": false
+            },
+            "test_results_manager_url": "https:\\/\\/test-results-manager.com",
+            "test_results_manager_expiration": 1234567890,
+            "test_summary": "Errors: 0, File Errors: 2",
+            "debug_log": "",
+            "version": "Undefined",
+            "update_complete": true,
+            "ai_suggestion_status": "none",
+            "malware_whitelist_paths": [],
+            "workflow_id": "1234567890",
+            "runner": "normalized",
+            "test_media": [],
+            "extension_set": "",
+            "phpstan_level": 2,
+            "test_variation": "",
+            "test_result_json_extracted": "{EXTRACTED}"
+        },
+        {
+            "test_result_json": {
+                "totals": {
+                    "errors": 0,
+                    "file_errors": 2
+                },
+                "files": {
+                    "\\/home\\/runner\\/work\\/qit-runner\\/qit-runner\\/ci\\/plugins\\/woocommerce-product-feeds\\/woocommerce-product-feeds.php": {
+                        "errors": 2,
+                        "messages": [
+                            {
+                                "message": "Function some_custom_wp_function not found.",
+                                "line": 12,
+                                "ignorable": true,
+                                "tip": "Learn more at https:\\/\\/phpstan.org\\/user-guide\\/discovering-symbols",
+                                "identifier": "function.notFound"
+                            },
+                            {
+                                "message": "Function totally_unknown_function not found.",
+                                "line": 15,
+                                "ignorable": true,
+                                "tip": "Learn more at https:\\/\\/phpstan.org\\/user-guide\\/discovering-symbols",
+                                "identifier": "function.notFound"
+                            }
+                        ]
+                    }
+                },
+                "errors": []
+            }
+        }
+    ]
+]';
