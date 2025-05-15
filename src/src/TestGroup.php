3461e8afa6304e71d32c42e61f07f601eb5be55d
MrJnrman <jnoelreid@gmai.com>
2025-05-15 15:15:22 -0500

    Display group when clearing

--- src/src/TestGroup.php
+++ src/src/TestGroup.php
@@ -46,10 +46,11 @@ class TestGroup {
 	 *
 	 * @param array<string, mixed>  $options The options for the test.
 	 * @param string                $test_type The type of test.
+	 * @param OutputInterface       $output The output.
 	 * @param InputInterface|null   $input The input.
 	 * @param array<string, string> $env_vars The environment variables.
 	 */
-	public function create_or_update( array $options, string $test_type, ?InputInterface $input = null, array $env_vars = [] ): void {
+	public function create_or_update( array $options, string $test_type, OutputInterface $output, ?InputInterface $input = null, array $env_vars = [] ): void {
 		$group = $this->cache->get( 'group' );
 
 		if (
@@ -67,7 +68,11 @@ class TestGroup {
 		}
 
 		if ( $this->pending_test_group_exists() ) {
-			throw new \Exception( 'A pending test group already exists. Either wait for it to complete or delete it with `group:clear`.' );
+			$output->writeln( '<comment>A pending test group already exists. It will be displayed and clear in order to complete your action.</comment>' );
+			$this->output_group( $output );
+
+			$this->cache->delete( 'group' );
+			$group = [];
 		}
 
 		if ( empty( $group ) ) {
@@ -534,5 +539,36 @@ class TestGroup {
 			->request();
 
 		return json_decode( $response, true );
+	}
+
+	public function output_group( OutputInterface $output ): void {
+		$group = $this->get();
+		if ( ! array_key_exists( 'group_identifier', $group ) ) {
+			return;
+		}
+
+		$output->writeln( '--------------------------------' );
+		$output->writeln( sprintf( '<info>Group Identifier: %s</info>', $group['group_identifier'] ) );
+		$output->writeln( '--------------------------------' );
+
+		if ( array_key_exists( 'test_runs', $group ) ) {
+			foreach ( $group['test_runs'] as $test_run ) {
+				$output->writeln( sprintf( '<info>Test Run ID: %s</info>', $test_run['test_run_id'] ) );
+				$output->writeln( sprintf( '<info>Woo ID: %s</info>', $test_run['woo_extension']['id'] ) );
+				$output->writeln( sprintf( '<info>Extension Name: %s</info>', $test_run['woo_extension']['name'] ) );
+				$output->writeln( sprintf( '<info>Test Type: %s</info>', $test_run['test_type_display'] ) );
+				$output->writeln( sprintf( '<info>Test Results Manager URL: %s</info>', $test_run['test_results_manager_url'] ) );
+				$output->writeln( sprintf( '<info>Test Run Status: %s</info>', $test_run['status'] ) );
+				$output->writeln( '--------------------------------' );
+			}
+		} else {
+			foreach ( $group['tests'] as $test ) {
+				$output->writeln( sprintf( '<info>Test Run ID: %s</info>', $test['test_run']['test_run_id'] ) );
+				$output->writeln( sprintf( '<info>Woo ID: %s</info>', $test['test_run']['woo_id'] ) );
+				$output->writeln( sprintf( '<info>Extension Slug: %s</info>', $test['test_run']['slug'] ) );
+				$output->writeln( sprintf( '<info>Test Type: %s</info>', $test['test_run']['test_type_display'] ) );
+				$output->writeln( '--------------------------------' );
+			}
+		}
 	}
 }
