e61f42e34cf70dfed975ca148e651cde6df45140
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-07-30 16:23:01 -0300

    Add plugins for tests

---
+++ src/tests/integration/data/plugins/cli-plugin/cli-plugin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: CLI Plugin
+Description: A stub plugin for testing CLI overrides.
+Version: 1.0.0
+Author: QIT Test
+*/
---
+++ src/tests/integration/data/plugins/config-plugin/config-plugin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Config Plugin
+Description: A stub plugin for testing configuration overrides.
+Version: 1.0.0
+Author: QIT Test
+*/
