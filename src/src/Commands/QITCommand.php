c22830836858296d1962883d7e05f1636b63432b
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-09-04 13:38:20 -0300

    Add experimental flag if using qit.json

--- src/src/Commands/QITCommand.php
+++ src/src/Commands/QITCommand.php
@@ -103,6 +103,12 @@ abstract class QITCommand extends Command {
 		if ( $config_file === null && file_exists( getcwd() . '/qit.json' ) ) {
 			$config_file = getcwd() . '/qit.json';
 		}
+		
+		// Display experimental warning when using qit.json
+		if ( $config_file !== null && $this->output !== null ) {
+			$this->output->writeln( '<comment>[EXPERIMENTAL]</comment> Using qit.json - this feature is highly experimental. Please report any issues or feedback at https://github.com/woocommerce/qit-cli/issues' );
+			$this->output->writeln( '' );
+		}
 
 		return $config_file;
 	}
