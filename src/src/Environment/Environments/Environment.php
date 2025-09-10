30a0ea7cdf7e25e3bcfa9b65bb0bb4bc5f02f88a
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-09-10 11:23:23 -0300

    Add safety check for docker-compose file in environment startup

--- src/src/Environment/Environments/Environment.php
+++ src/src/Environment/Environments/Environment.php
@@ -395,7 +395,11 @@ abstract class Environment {
 
 		if ( ! $up_process->isSuccessful() ) {
 			static::down( $this->env_info );
-			$this->output->writeln( file_get_contents( $this->env_info->temporary_env . '/docker-compose.yml' ) );
+			// Check if docker-compose.yml exists before trying to read it
+			$compose_file = $this->env_info->temporary_env . '/docker-compose.yml';
+			if ( file_exists( $compose_file ) ) {
+				$this->output->writeln( file_get_contents( $compose_file ) );
+			}
 			throw new \RuntimeException( "Failed to start the environment. Output: \n" . $up_process->getOutput() . $up_process->getErrorOutput() );
 		}
 
