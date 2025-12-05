ec759e5fdef9d6f76d64d6b3c2030ca1724c2dc6
Alex López <alex.lopez@automattic.com>
2025-12-05 17:19:47 +0100

    Update managed tests snapshots.

--- _tests/managed_tests/tests/__snapshots__/PhpstanTest__test_phpstan_childtheme_woo_php_wp_acd1df3b6787d2c2218b6be0b465f886__1.php
+++ _tests/managed_tests/tests/__snapshots__/PhpstanTest__test_phpstan_childtheme_woo_php_wp_acd1df3b6787d2c2218b6be0b465f886__1.php
@@ -46,7 +46,6 @@
             "phpstan_level": 2,
             "test_variation": "",
             "test_packages": [],
-            "iterations": 7,
             "test_group_id": "",
             "created_at": "2025-01-01 00:00:00",
             "test_result_json_extracted": "{EXTRACTED}"
