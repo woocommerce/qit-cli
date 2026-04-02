4771358e2a8ffb38a56ce5fbdfd7ecaa2b283798
Lucas Bustamante <lucasfbustamante@gmail.com>
2026-04-02 18:18:28 -0300

    Update activation snapshots

--- _tests/managed_tests/tests/__snapshots__/ActivationTest__test_activation_childtheme_woostable_php82_wpstable_586f8c4da7220df02f5f27d4e54835e3__1.php
+++ _tests/managed_tests/tests/__snapshots__/ActivationTest__test_activation_childtheme_woostable_php82_wpstable_586f8c4da7220df02f5f27d4e54835e3__1.php
@@ -110,7 +110,7 @@
                                 "packageId": "woocommerce\\/activation:latest",
                                 "testType": "e2e",
                                 "exitCode": 0,
-                                "output": "Notice: Function _load_textdomain_just_in_time was called <strong>incorrectly<\\/strong>. Translation loading for the <code>woocommerce<\\/code> domain was triggered too early. This is usually an indicator for some code in the plugin or theme running too early. Translations should be loaded at the <code>init<\\/code> action or later. Please see <a href=\\"https:\\/\\/developer.wordpress.org\\/advanced-administration\\/debug\\/debug-wordpress\\/\\">Debugging in WordPress<\\/a> for more information. (This message was added in version 6.7.0.) in \\/var\\/www\\/html\\/wp-includes\\/functions.php on line {LINE}\\nSuccess: Updated \'woocommerce_onboarding_profile_completed\' option.",
+                                "output": "Success: Updated \'woocommerce_onboarding_profile_completed\' option.",
                                 "isLifecycle": true,
                                 "countsTowardTotals": false,
                                 "packageType": "test",
