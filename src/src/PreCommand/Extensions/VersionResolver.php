ff4ffa63d799833cf9ec9ac9ae3eddf824d83942
Greg <greg.bell@automattic.com>
2026-04-29 16:20:48 -0600

    Use api instead of woo-api

--- src/src/PreCommand/Extensions/VersionResolver.php
+++ src/src/PreCommand/Extensions/VersionResolver.php
@@ -23,6 +23,10 @@ class VersionResolver {
 				return 'https://github.com/woocommerce/woocommerce/releases/download/nightly/woocommerce-trunk-nightly.zip';
 
 			default:
+				if ( preg_match( '/^\d+\.\d+\.\d+-dev$/', $version ) ) {
+					return "https://github.com/woocommerce/woocommerce/releases/download/{$version}/woocommerce.zip";
+				}
+
 				return null; // Not a special version we handle
 		}
 	}
